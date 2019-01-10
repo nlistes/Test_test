cbc0b2eedfcdffa11dd47829450f496db8cec74b
nlistes <aa@bb.sk>
2019-01-10 23:25:02 +0200

    test2 added

---
+++ Sketch1/Sketch2/Sketch2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch2.ino
+ Created:	1/10/2019 11:24:41 PM
+ Author:	Lietotajs
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
