11bb07ab6fa29b9df867a15f4864ed94e5cf37e6
Pukinens <pjtube@gmail.com>
2019-01-10 23:18:27 +0200

    test project 1

---
+++ Sketch1/Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	1/10/2019 11:17:56 PM
+ Author:	Lietotajs
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
